601b5732e6ffb4df4cf03806c029d8127b86d437
unknown <kundankumar.harina@gmail.com>
2021-02-19 20:17:57 +0530

    checking issues

--- src/Components/Body/Banner.js
+++ src/Components/Body/Banner.js
@@ -5,7 +5,6 @@ const Banner = () => {
     return (
         <section className="bodyBanner" style={{marginLeft: '15%'}}>
                     <img src={banner} alt="banner-1"/>
-                    {/* kundanjknjk Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi veniam porro temporibus accusamus inventore quisquam ab. Placeat ea laborum veritatis error tempora excepturi dolores neque ratione mollitia, possimus harum! Ab. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates tempore alias distinctio atque officiis, ducimus minus. Id ipsam, harum omnis suscipit nihil mollitia molestiae recusandae voluptatum accusamus quo quam. */}
         </section>
     );
 }
